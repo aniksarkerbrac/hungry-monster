4576026851afc395eef9834ba7fc3bcd600b2b51
Anik Sarker <anik.brac.2016@gmail.com>
2021-07-31 01:31:44 +0600

    done hungrey moster using api

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const searchMeal = () => {
 const showMeal = allMeals => {
     const mealsDiv = document.getElementById('meals');
     const error = document.getElementById('error-msg');
+    const mealDetailDiv = document.getElementById('meal-detail');
+    mealDetailDiv.innerHTML = " ";
     mealsDiv.innerHTML = " ";
     error.innerHTML = " ";
 
@@ -22,16 +24,36 @@ const showMeal = allMeals => {
             const mealDiv = document.createElement('div');
             mealDiv.id = 'meal';
             const mealInfo = `
-                <img src="${meal.strMealThumb}">
-                <h5>${meal.strMeal}</h5>                
+                <div onclick="showDetailInfo('${meal.strMeal}')">
+                    <img src="${meal.strMealThumb}">
+                    <h5>${meal.strMeal}</h5>                              
+                </div>
             `
             mealDiv.innerHTML = mealInfo;
             mealsDiv.appendChild(mealDiv);
-            document.getElementById('meal').onclick ="showDetailInfo('${meal.strMeal}')";
         })
     }
 }
-//<button onclick="showDetailInfo('${meal.strMeal}')">click</button>
-const showDetailInfo = meal =>{
-    console.log(meal);
+ 
+const showDetailInfo = mealName =>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+        .then(res => res.json())
+        .then(data =>{
+            const meal = data.meals[0];
+            const mealDetailDiv = document.getElementById('meal-detail');
+            const detail =`
+                <img src="${meal.strMealThumb}">
+                <h3>${meal.strMeal}</h3> 
+                <h6>Ingredients</h6>
+                <ul>
+                    <li>${meal.strIngredient1}</li>
+                    <li>${meal.strIngredient2}</li>
+                    <li>${meal.strIngredient3}</li>
+                    <li>${meal.strIngredient4}</li>
+                    <li>${meal.strIngredient5}</li>
+                    <li>${meal.strIngredient6}</li>
+                </ul>
+            `
+            mealDetailDiv.innerHTML = detail;
+        })   
 }
